437777b8e82f62abda8b0fc2594202ae73efd234
iammarix <iammarix@gmail.com>
2018-02-06 13:40:48 -0500

    comments

--- gradejs/index.js
+++ gradejs/index.js
@@ -1,11 +1,24 @@
 (function () {
+	// require our grading module
 	var grade = require('./grade');
 
-	var scores = [99, 88, 86, 77, 75, 66, 55, 40, 38, 31, 22];
+	// array of scores to grade
+	// result is printed to console
+	// todo: edit array for custom scoring
+	var scores = [99, 92, 91, 91, 89, 85, 83, 82, 80, 79, 78, 78, 77, 76, 75, 74, 62, 55, 43, 20];
+
+
 	grade(scores, function (err, grades) {
 		if (!err) {
-			console.log(grades);
+			// print resulting grades to console
+			console.log("A: "+grades.A);
+			console.log("B: "+grades.B);
+			console.log("C: "+grades.C);
+			console.log("D: "+grades.D);
+			console.log("F: "+grades.F);
+
 		} else {
+			// print an error if we got one
 			console.error(err);
 		}
 	});
