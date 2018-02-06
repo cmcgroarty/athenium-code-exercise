794d701a1e048d3138da12c3c10fb3f1553af517
iammarix <iammarix@gmail.com>
2018-02-06 15:06:20 -0500

    readme instructions

--- sql/README.md
+++ sql/README.md
@@ -1,4 +1,12 @@
 # sql
 
-## Result
+## Getting started
+
+### Init Tables with Dummy Content
+Run `athenium-exercise-init.sql` to init a db with tables and some dummy content
+
+### Run query
+Run `athenium-exercise-orderitems.sql` to find Item Name & Category Name
+
+### Result
 ![SQL Query Result](https://github.com/iammarix/athenium-code-exercise/blob/master/sql/queryresult.PNG)
--- sql/athenium-exercise-orderitems.sql
+++ sql/athenium-exercise-orderitems.sql
@@ -1,3 +1,3 @@
-SELECT iom.orderID AS 'Order ID', i.name AS 'Item Name', c.name AS 'Category Name' FROM `Item` AS i, `Category` AS c, ItemOrderMembership as iom
+SELECT iom.orderID AS 'Order ID', i.name AS 'Item Name', c.name AS 'Category Name' FROM `Item` AS i, `Category` AS c, `ItemOrderMembership` as iom
 WHERE (i.categoryID=c.categoryID) AND  i.itemID IN
 (SELECT iom.itemID FROM `ItemOrderMembership` WHERE iom.orderID=12345)
